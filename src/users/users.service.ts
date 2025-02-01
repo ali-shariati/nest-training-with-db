872428737f347f77d8cf3be37fb3829cc7eb4a90
Ali Shariati <seyed.ali.shariati@gmail.com>
2025-02-01 18:44:28 +0330

    add crud fn to user service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,44 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectRepository(User) private repo: Repository<User>) {}
+
+  create(email: string, password: string) {
+    if (!email || !password) {
+      throw new Error('Email and password cannot be empty');
+    }
+    const user = this.repo.create({ email, password });
+    return this.repo.save(user);
+  }
+
+
+  findOne(id: number) {
+    return this.repo.findOne({ where: { id } });
+  }
+
+  find(email: string) {
+    return this.repo.findOne({ where: { email } });
+  }
+
+  async update(id:number, attrs:Partial<User>){
+    const user = await this.findOne(id);
+
+    if(!user){
+      throw new Error('User Not Found')
+    }
+
+    Object.assign(user,attrs);
+    return this.repo.save(user)
+  }
+
+  async remove(id:number){
+    const user = await this.findOne(id);
+    if(!user) throw new Error('User Not Found');
+
+    return this.repo.remove(user);
+  }
+}
