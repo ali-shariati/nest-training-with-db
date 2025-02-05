b5e8106ebb19075b5f44d9d8d490a7815155bfad
Ali Shariati <seyed.ali.shariati@gmail.com>
2025-02-05 23:10:22 +0330

    add user serialize function to user controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,13 +7,16 @@ import {
   Delete,
   Param,
   Query,
-  NotFoundException, BadRequestException,
+  NotFoundException, BadRequestException, UseInterceptors,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
+import { Serialize, SerializeInterceptor } from '../serialize.interceptor';
+import { UserDto } from './dtos/user.dto';
 
 @Controller('auth')
+@Serialize(UserDto)
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
