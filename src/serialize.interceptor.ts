251746511e36f3dc33a48b4a9e7ee42f338cf970
Ali Shariati <seyed.ali.shariati@gmail.com>
2025-02-05 23:08:29 +0330

    add user serialize

---
+++ src/serialize.interceptor.ts
@@ -0,0 +1,25 @@
+import { CallHandler, ExecutionContext, NestInterceptor, UseInterceptors } from '@nestjs/common';
+import { map, Observable } from 'rxjs';
+import { plainToClass } from 'class-transformer';
+
+interface ClassConstructor {
+  new (...arg:any[]):{};
+}
+export function Serialize(dto:ClassConstructor){
+  return UseInterceptors(new SerializeInterceptor(dto))
+}
+
+export class SerializeInterceptor implements NestInterceptor{
+  constructor(private dto:any) {
+  }
+  intercept(context: ExecutionContext, handler: CallHandler): Observable<any>{
+    console.log("I am running before the handler", context);
+    return handler.handle().pipe(
+      map((data: any) => {
+        return plainToClass(this.dto, data, {
+          excludeExtraneousValues: true
+        })
+      }),
+    )
+  }
+}
